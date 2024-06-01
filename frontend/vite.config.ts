8d47f8cb30e93a1562c78947700ea0498a6bb1ca
Rishabh Prajapati <prajapatirishabh04@gmail.com>
2024-06-01 03:12:41 -0400

    vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://quiz.iamrishabh.tech:8000',
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
 
 
